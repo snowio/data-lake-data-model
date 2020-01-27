5845d75d376a5bf462cb456d137b1750211c2bc0
Alexander Wanyoike <wanyoike.alexander@gmail.com>
2020-01-27 09:06:21 +0000

    Formatting: Remove space

--- src/Commands/SaveSegmentCommand.php
+++ src/Commands/SaveSegmentCommand.php
@@ -6,7 +6,6 @@ use SnowIO\DataLakeDataModel\Segment;
 
 class SaveSegmentCommand extends Command
 {
-
     public function toJson(): array
     {
         return array_merge(parent::toJson(), $this->segment->toJson(Segment::SAVE));
